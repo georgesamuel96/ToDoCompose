1e1b6dbd6cd4c8102d60e41addd4339ce4a18b38
George Samuel <georges012441@gmail.com>
2024-12-21 11:03:00 -0500

    handle system back button navigation

--- app/build/tmp/kapt3/stubs/debug/com/example/to_docompose/ui/screens/task/TaskScreenKt.java
+++ app/build/tmp/kapt3/stubs/debug/com/example/to_docompose/ui/screens/task/TaskScreenKt.java
@@ -1,14 +1,18 @@
 package com.example.to_docompose.ui.screens.task;
 
 import android.content.Context;
+import android.util.Log;
 import android.widget.Toast;
+import androidx.activity.OnBackPressedCallback;
+import androidx.activity.OnBackPressedDispatcher;
+import androidx.activity.compose.LocalOnBackPressedDispatcherOwner;
 import androidx.compose.runtime.Composable;
 import androidx.compose.ui.Modifier;
 import com.example.to_docompose.data.models.ToDoTask;
 import com.example.to_docompose.ui.viewmodels.SharedViewModel;
 import com.example.to_docompose.util.Action;
 
-@kotlin.Metadata(mv = {1, 9, 0}, k = 2, xi = 48, d1 = {"\u0000&\n\u0000\n\u0002\u0010\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\b\u0002\n\u0002\u0018\u0002\n\u0000\u001a.\u0010\u0000\u001a\u00020\u00012\b\u0010\u0002\u001a\u0004\u0018\u00010\u00032\u0006\u0010\u0004\u001a\u00020\u00052\u0012\u0010\u0006\u001a\u000e\u0012\u0004\u0012\u00020\b\u0012\u0004\u0012\u00020\u00010\u0007H\u0007\u001a\u000e\u0010\t\u001a\u00020\u00012\u0006\u0010\n\u001a\u00020\u000b\u00a8\u0006\f"}, d2 = {"TaskScreen", "", "selectedTask", "Lcom/example/to_docompose/data/models/ToDoTask;", "sharedViewModel", "Lcom/example/to_docompose/ui/viewmodels/SharedViewModel;", "navigateToListScreen", "Lkotlin/Function1;", "Lcom/example/to_docompose/util/Action;", "displayToast", "context", "Landroid/content/Context;", "app_debug"})
+@kotlin.Metadata(mv = {1, 9, 0}, k = 2, xi = 48, d1 = {"\u00004\n\u0000\n\u0002\u0010\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0002\b\u0002\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\b\u0002\n\u0002\u0018\u0002\n\u0000\u001a\"\u0010\u0000\u001a\u00020\u00012\n\b\u0002\u0010\u0002\u001a\u0004\u0018\u00010\u00032\f\u0010\u0004\u001a\b\u0012\u0004\u0012\u00020\u00010\u0005H\u0007\u001a.\u0010\u0006\u001a\u00020\u00012\b\u0010\u0007\u001a\u0004\u0018\u00010\b2\u0006\u0010\t\u001a\u00020\n2\u0012\u0010\u000b\u001a\u000e\u0012\u0004\u0012\u00020\r\u0012\u0004\u0012\u00020\u00010\fH\u0007\u001a\u000e\u0010\u000e\u001a\u00020\u00012\u0006\u0010\u000f\u001a\u00020\u0010\u00a8\u0006\u0011"}, d2 = {"BackHandler", "", "backDispatcher", "Landroidx/activity/OnBackPressedDispatcher;", "onBackPressed", "Lkotlin/Function0;", "TaskScreen", "selectedTask", "Lcom/example/to_docompose/data/models/ToDoTask;", "sharedViewModel", "Lcom/example/to_docompose/ui/viewmodels/SharedViewModel;", "navigateToListScreen", "Lkotlin/Function1;", "Lcom/example/to_docompose/util/Action;", "displayToast", "context", "Landroid/content/Context;", "app_debug"})
 public final class TaskScreenKt {
     
     @androidx.compose.runtime.Composable()
@@ -20,5 +24,11 @@ public final class TaskScreenKt {
     
     public static final void displayToast(@org.jetbrains.annotations.NotNull()
     android.content.Context context) {
+    }
+    
+    @androidx.compose.runtime.Composable()
+    public static final void BackHandler(@org.jetbrains.annotations.Nullable()
+    androidx.activity.OnBackPressedDispatcher backDispatcher, @org.jetbrains.annotations.NotNull()
+    kotlin.jvm.functions.Function0<kotlin.Unit> onBackPressed) {
     }
 }
--- app/src/main/java/com/example/to_docompose/ui/screens/task/TaskScreen.kt
+++ app/src/main/java/com/example/to_docompose/ui/screens/task/TaskScreen.kt
@@ -2,10 +2,16 @@ package com.example.to_docompose.ui.screens.task
 
 import android.content.Context
 import android.widget.Toast
+import androidx.activity.OnBackPressedCallback
+import androidx.activity.OnBackPressedDispatcher
+import androidx.activity.compose.LocalOnBackPressedDispatcherOwner
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberUpdatedState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import com.example.to_docompose.data.models.ToDoTask
@@ -23,15 +29,19 @@ fun TaskScreen(
     val priority by sharedViewModel.priority
     val context = LocalContext.current
 
+    BackHandler(
+        onBackPressed = { navigateToListScreen(Action.NO_ACTION) }
+    )
+
     Scaffold(
         topBar = {
             TaskAppBar(
                 selectedTask = selectedTask,
                 navigateToListScreen = { action ->
-                    if(action == Action.NO_ACTION) {
+                    if (action == Action.NO_ACTION) {
                         navigateToListScreen(action)
                     } else {
-                        if(sharedViewModel.validateFields()) {
+                        if (sharedViewModel.validateFields()) {
                             navigateToListScreen(action)
                         } else {
                             displayToast(context = context)
@@ -62,4 +72,29 @@ fun TaskScreen(
 
 fun displayToast(context: Context) {
     Toast.makeText(context, "Fields Empty", Toast.LENGTH_SHORT).show()
+}
+
+@Composable
+fun BackHandler(
+    backDispatcher: OnBackPressedDispatcher? =
+        LocalOnBackPressedDispatcherOwner.current?.onBackPressedDispatcher,
+    onBackPressed: () -> Unit
+) {
+    val currentOnBackPresses by rememberUpdatedState(newValue = onBackPressed)
+
+    val backCallback = remember {
+        object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                currentOnBackPresses()
+            }
+        }
+    }
+
+    DisposableEffect(key1 = backDispatcher) {
+        backDispatcher?.addCallback(backCallback)
+
+        onDispose{
+            backCallback.remove()
+        }
+    }
 }
