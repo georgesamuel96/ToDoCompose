994c2cad564ef26b4a17787ca2cd5269c8260173
George Samuel <georges012441@gmail.com>
2024-12-21 13:03:16 -0500

    Delete app/build directory

--- app/build/generated/hilt/component_trees/debug/dagger/hilt/internal/processedrootsentinel/codegen/_com_example_to_docompose_ToDoApplication.java
+++
@@ -1,9 +0,0 @@
-package dagger.hilt.internal.processedrootsentinel.codegen;
-
-import dagger.hilt.internal.processedrootsentinel.ProcessedRootSentinel;
-
-@ProcessedRootSentinel(
-    roots = "com.example.to_docompose.ToDoApplication"
-)
-public final class _com_example_to_docompose_ToDoApplication {
-}
